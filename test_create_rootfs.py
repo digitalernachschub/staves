7860392bfe2841807b77df619e670ef018ac992f
Michael Seifert <m.seifert@digitalernachschub.de>
2017-11-20 10:59:41 +0100

    Unit tests can be run as an unprivileged user.

--- test_create_rootfs.py
+++ test_create_rootfs.py
@@ -3,21 +3,27 @@ import os
 from create_rootfs import create_rootfs
 
 
-def test_creates_lib_symlink(tmpdir):
+def test_creates_lib_symlink(tmpdir, monkeypatch):
+    unprivileged_test_root = tmpdir.join('test_root')
+    monkeypatch.setenv('ROOT', unprivileged_test_root)
     rootfs_path = tmpdir.join('rootfs')
     create_rootfs(rootfs_path, 'virtual/libintl')
 
     assert os.path.islink(os.path.join(rootfs_path, 'usr', 'lib'))
 
 
-def test_copies_libgcc(tmpdir):
+def test_copies_libgcc(tmpdir, monkeypatch):
+    unprivileged_test_root = tmpdir.join('test_root')
+    monkeypatch.setenv('ROOT', unprivileged_test_root)
     rootfs_path = tmpdir.join('rootfs')
     create_rootfs(rootfs_path, 'virtual/libintl')
 
     assert os.path.exists(os.path.join(rootfs_path, 'usr', 'lib64', 'libgcc_s.so.1'))
 
 
-def test_creates_locale_gen(tmpdir):
+def test_creates_locale_gen(tmpdir, monkeypatch):
+    unprivileged_test_root = tmpdir.join('test_root')
+    monkeypatch.setenv('ROOT', unprivileged_test_root)
     rootfs_path = tmpdir.join('rootfs')
     create_rootfs(rootfs_path, 'virtual/libintl')
 
