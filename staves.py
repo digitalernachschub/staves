9ea6dc45db10488dc37e2bf3661444d69b209239
Michael Seifert <m.seifert@digitalernachschub.de>
2018-03-19 09:24:57 +0100

    The locale for glibc is configurable. The default locale is "C" with charset "UTF-8".

--- staves.py
+++ staves.py
@@ -128,6 +128,7 @@ def main(version, libc, name):
             subprocess.run(['emaint', 'sync', '--repo', repository['name']], stderr=subprocess.PIPE)
     if 'repositories' in config:
         config.pop('repositories')
+    locale = config.pop('locale') if 'locale' in config else {'name': 'C', 'charset': 'UTF-8'}
     package_configs = {k: v for k, v in config.items() if isinstance(v, dict)}
     for package, package_config in package_configs.items():
         _write_package_config(package, **package_config)
@@ -142,7 +143,7 @@ def main(version, libc, name):
     _create_rootfs(rootfs_path, *packages_to_be_installed, symlink_lib=symlink_lib)
     if 'glibc' in libc:
         with open(os.path.join('/etc', 'locale.gen'), 'a') as locale_conf:
-            locale_conf.writelines(['en_US.UTF-8 UTF-8'])
+            locale_conf.writelines('{} {}'.format(locale['name'], locale['charset']))
             subprocess.run('locale-gen')
         os.makedirs(os.path.join(rootfs_path, 'usr', 'lib', 'locale'), exist_ok=True)
         shutil.copy(os.path.join('/usr', 'lib', 'locale', 'locale-archive'), os.path.join(rootfs_path, 'usr', 'lib', 'locale'))
