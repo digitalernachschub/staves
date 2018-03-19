0a0e85aa3d9f882dc479dfef40197b48a53e541f
Michael Seifert <m.seifert@digitalernachschub.de>
2018-03-19 09:58:53 +0100

    Fixed test_copies_libgcc.

--- test_staves.py
+++ test_staves.py
@@ -27,12 +27,20 @@ def test_creates_lib_symlink(tmpdir, monkeypatch, mocker):
     assert os.path.islink(os.path.join(rootfs_path, 'usr', 'local', 'lib'))
 
 
-def test_copies_libgcc(tmpdir, monkeypatch):
+def test_copies_libgcc(tmpdir, monkeypatch, mocker):
     package_dir = tmpdir.join('packages')
     monkeypatch.setenv('PKGDIR', package_dir)
     unprivileged_test_root = tmpdir.join('test_root')
     monkeypatch.setenv('ROOT', unprivileged_test_root)
     rootfs_path = tmpdir.join('rootfs')
-    _create_rootfs(rootfs_path, 'virtual/libintl')
+    config = toml.dumps(dict(
+        name='staves_test',
+        packages=['virtual/libintl'],
+        command=''
+    ))
+    mocker.patch('staves._docker_image_from_rootfs')
+    cli = CliRunner()
+
+    cli.invoke(main, input=config, args=['--rootfs_path', rootfs_path, 'latest'])
 
     assert os.path.exists(os.path.join(rootfs_path, 'usr', 'lib64', 'libgcc_s.so.1'))
