45f0aaa136d07042ee11a5a9bfe5ebdb467ffe3e
Michael Seifert <m.seifert@digitalernachschub.de>
2018-03-28 11:18:45 +0200

    Removed os.path.join from some paths to improve readability. It is not expected that the file paths need to be cross-platform at some point in the future.

--- staves.py
+++ staves.py
@@ -194,9 +194,9 @@ def main(version, libc, name, rootfs_path, packaging, create_builder):
         os.makedirs(os.path.join(rootfs_path, 'usr', 'lib', 'locale'), exist_ok=True)
         shutil.copy(os.path.join('/usr', 'lib', 'locale', 'locale-archive'), os.path.join(rootfs_path, 'usr', 'lib', 'locale'))
     if create_builder:
-        _copy_to_rootfs(rootfs_path, os.path.join('/usr', 'portage'))
-        _copy_to_rootfs(rootfs_path, os.path.join('/etc', 'portage', 'make.conf'))
-        _copy_to_rootfs(rootfs_path, os.path.join('/etc', 'portage', 'make.profile'))
+        _copy_to_rootfs(rootfs_path, '/usr/portage')
+        _copy_to_rootfs(rootfs_path, '/etc/portage/make.conf')
+        _copy_to_rootfs(rootfs_path, '/etc/portage/make.profile')
     tag = '{}:{}'.format(name, version)
     if packaging == 'docker':
         _docker_image_from_rootfs(rootfs_path, tag, config['command'])
