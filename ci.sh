feb6b0e66e0282fc3db1047ad4faa31fce3294de
Michael Seifert <m.seifert@digitalernachschub.de>
2019-02-01 14:57:47 +0100

    Deactivated musl toolchain due to a build failure.

--- ci.sh
+++ ci.sh
@@ -32,18 +32,18 @@ setup_test_env
 run_unit_tests
 
 portage_snapshot="20190127"
-musl_stage3_date="20190104"
-create_stage3_image ${musl_stage3_date}
-full_version="${version}.${musl_stage3_date}"
-python3 -m staves init --runtime docker --staves-version "${full_version}" --libc musl --stage3 "${musl_stage3_date}" --portage-snapshot "${portage_snapshot}"
-python3 -m staves build --runtime docker --runtime-docker-build-cache staves-x86_64-musl-cache \
-    --runtime-docker-builder "staves/bootstrap-x86_64-musl:${full_version}" --create-builder --libc "sys-libs/musl" \
-    --config x86_64-musl.toml "${full_version}"
-if [[ $(git tag --list ${project_name}-${version}) ]]; then
-  docker tag "staves/x86_64-musl:${full_version}" "staves/x86_64-musl:${version}"
-  docker tag "staves/x86_64-musl:${full_version}" "staves/x86_64-musl:${version%.*}"
-  docker tag "staves/x86_64-musl:${full_version}" "staves/x86_64-musl:${version%%.*}"
-fi
+#musl_stage3_date="20190104"
+#create_stage3_image ${musl_stage3_date}
+#full_version="${version}.${musl_stage3_date}"
+#python3 -m staves init --runtime docker --staves-version "${full_version}" --libc musl --stage3 "${musl_stage3_date}" --portage-snapshot "${portage_snapshot}"
+#python3 -m staves build --runtime docker --runtime-docker-build-cache staves-x86_64-musl-cache \
+#    --runtime-docker-builder "staves/bootstrap-x86_64-musl:${full_version}" --create-builder --libc "sys-libs/musl" \
+#    --config x86_64-musl.toml "${full_version}"
+#if [[ $(git tag --list ${project_name}-${version}) ]]; then
+#  docker tag "staves/x86_64-musl:${full_version}" "staves/x86_64-musl:${version}"
+#  docker tag "staves/x86_64-musl:${full_version}" "staves/x86_64-musl:${version%.*}"
+#  docker tag "staves/x86_64-musl:${full_version}" "staves/x86_64-musl:${version%%.*}"
+#fi
 
 glibc_stage3_date="20190104"
 full_version="${version}.${glibc_stage3_date}"
