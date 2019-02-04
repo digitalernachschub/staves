79a15ecee3efe3bf0b418c4d0d6a2d06d1eb584f
Michael Seifert <m.seifert@digitalernachschub.de>
2019-02-04 08:35:30 +0100

    Removed explicit runtime argument from CI build.

--- ci.sh
+++ ci.sh
@@ -36,7 +36,7 @@ portage_snapshot="20190127"
 #create_stage3_image ${musl_stage3_date}
 #full_version="${version}.${musl_stage3_date}"
 #python3 -m staves init --runtime docker --staves-version "${full_version}" --libc musl --stage3 "${musl_stage3_date}" --portage-snapshot "${portage_snapshot}"
-#python3 -m staves build --runtime docker --runtime-docker-build-cache staves-x86_64-musl-cache \
+#python3 -m staves build --runtime-docker-build-cache staves-x86_64-musl-cache \
 #    --runtime-docker-builder "staves/bootstrap-x86_64-musl:${full_version}" --create-builder --libc "sys-libs/musl" \
 #    --config x86_64-musl.toml "${full_version}"
 #if [[ $(git tag --list ${project_name}-${version}) ]]; then
@@ -48,7 +48,7 @@ portage_snapshot="20190127"
 glibc_stage3_date="20190104"
 full_version="${version}.${glibc_stage3_date}"
 python3 -m staves init --runtime docker --staves-version "${full_version}" --libc glibc --stage3 "${glibc_stage3_date}" --portage-snapshot "${portage_snapshot}"
-python3 -m staves build --runtime docker --runtime-docker-build-cache staves-x86_64-glibc-cache  \
+python3 -m staves build --runtime-docker-build-cache staves-x86_64-glibc-cache  \
     --runtime-docker-builder "staves/bootstrap-x86_64-glibc:${full_version}" --create-builder --libc "sys-libs/glibc" \
     --config x86_64-glibc.toml "${full_version}"
 
