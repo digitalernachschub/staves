b9b40c50ee2b4b035e72fbd407f4e93772ec3ab2
Michael Seifert <m.seifert@digitalernachschub.de>
2020-10-21 11:14:50 +0200

    refactor: Moved helper functions from the docker packager into the cli.

--- staves/cli.py
+++ staves/cli.py
@@ -5,7 +5,7 @@ import logging
 import os
 import tarfile
 from pathlib import Path
-from typing import IO, MutableMapping, Any, Sequence
+from typing import IO, Mapping, MutableMapping, Any, Sequence
 
 import click
 import docker
@@ -21,7 +21,6 @@ from staves.builders.gentoo import (
     PackagingConfig,
     Repository,
 )
-from staves.packagers.docker import _create_dockerfile
 
 
 logger = logging.getLogger(__name__)
@@ -183,6 +182,37 @@ def _read_packaging_config(config_file: IO) -> PackagingConfig:
         annotations=data.get("annotations", {}),
         version=data.get("version"),
     )
+
+
+def _create_dockerfile(annotations: Mapping[str, str], *cmd: str) -> str:
+    label_string = " ".join(
+        [f'"{key}"="{value}"' for key, value in annotations.items()]
+    )
+    command_string = ", ".join(['"{}"'.format(c) for c in cmd])
+    dockerfile = ""
+    if label_string:
+        dockerfile += "LABEL {label_string}" + os.linesep
+    dockerfile += f"""\
+    FROM scratch
+    COPY rootfs /
+    ENTRYPOINT [{command_string}]
+    """
+    return dockerfile
+
+
+def _docker_image_from_rootfs(
+    rootfs_path: str, tag: str, command: Sequence, annotations: Mapping[str, str]
+):
+    client = docker.from_env()
+    dockerfile = _create_dockerfile(annotations, *command).encode("utf-8")
+    context = io.BytesIO()
+    with tarfile.open(fileobj=context, mode="w") as tar:
+        dockerfile_info = tarfile.TarInfo(name="Dockerfile")
+        dockerfile_info.size = len(dockerfile)
+        tar.addfile(dockerfile_info, fileobj=io.BytesIO(dockerfile))
+        tar.add(name=rootfs_path, arcname="rootfs")
+    context.seek(0)
+    client.images.build(fileobj=context, tag=tag, custom_context=True)
 
 
 def main():
